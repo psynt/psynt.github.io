5561efa4fb7625a73a81a17a673f7d6148c79f93
Nichita <tnichita@pm.me>
2023-05-24 18:16:21 +0900

    styling

--- ink/wishlist.js
+++
@@ -1,8 +0,0 @@
-var wishList = [
-    { "Priority":"2", "Brand":"Pilot Iroshizuku", "name": "Kon-Peki"},
-    { "Priority":"1", "Brand":"Diamine", "name": "Turqoise"},
-    { "Priority":"1", "Brand":"Pelikan", "name": "Edel - Topaz", "size":"s"},
-    { "Priority":"1", "Brand":"Private Reserve", "name": "Tropical Blue", "size" : "s"},
-    { "Priority":"1", "Brand":"Private Reserve", "name": "Daphne Blue", "size" : "s"},
-    { "Priority":"1", "Brand":"Private Reserve", "name": "Electric DC Blue", "size" : "s"}
-];
